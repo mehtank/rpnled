5092ad05f9fccf6a4c1c6efc8618721160cbe96d
mehtank <mehtank>
2016-12-24 02:39:01 -0800

    Moved LED to debug, added debug options

--- rpnled/debug.h
+++ rpnled/debug.h
@@ -2,9 +2,18 @@
 #define __DEBUG_H
 
 #  ifndef DEBUG_ENABLED
-#    define DEBUG(str, var) (void)0
+#    define DEBUG1(str) (void)0
+#    define DEBUG2(str, var) (void)0
 #  else
-#    define DEBUG(str, var) {Serial.print(str); Serial.println(var);}
+#    define DEBUG1(str) Serial.println(str)
+#    define DEBUG2(str, var) {Serial.print(str); Serial.println(var);}
 #  endif
+
+#  define GET_MACRO(_1,_2,NAME,...) NAME
+#  define DEBUG(...) GET_MACRO(__VA_ARGS__, DEBUG2, DEBUG1)(__VA_ARGS__)
+
+#  define LED_PIN D4
+#  define LED_ON digitalWrite(LED_PIN, LOW)
+#  define LED_OFF digitalWrite(LED_PIN, HIGH)
 
 #endif
