4a2656028a8454061a96c3ef643ac24cd45ab8ff
mehtank <mehtank>
2017-01-02 03:44:33 -0800

    Manual launch command

--- rpnled/rpnled.ino
+++ rpnled/rpnled.ino
@@ -253,6 +253,10 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * buffer, size_t rxc) {
 	    } else if (!strncmp((char*)buffer, "$FWSTOP", 7)) {
               state = FIREWORKS;
               stopFireworks();
+	    } else if (rxc == 6 && !strncmp((char*)buffer, "$FW", 3)) {
+              state = FIREWORKS;
+              stopFireworks();
+              launch(buffer[3], buffer[4], buffer[5]);
 	    } else if (!strncmp((char*)buffer, "$OFF", 4)) {
               fill_solid(leds, NUM_LEDS, CRGB::Black);
               state = REDRAW;
